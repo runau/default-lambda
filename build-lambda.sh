5db1a21c5bb9cabaff19d3dd6a328fb506100f12
runau <runa@engineergirl.work>
2020-04-02 22:01:27 +0900

    並び順が綺麗になるように名前変更

--- build-lambda.sh
+++ build-lambda.sh
@@ -6,11 +6,11 @@ env=$3
 # project=$5
 
 template_name="lambda"
-sed -e s/{{lambda_name}}/$lambda_name/ ./$template_name-template.yaml > ./$template_name-sed-template.yaml
+sed -e s/{{lambda_name}}/$lambda_name/ ./template-$template_name.yaml > ./template-$template_name-sed.yaml
 packaged_name="./packaged.yaml"
 stack_name="$template_name-`echo $lambda_name | sed s/_/-/`-$env"
 echo $stack_name
 param="Env=$env"
 # param="Env=$env Project=$project"
-aws cloudformation package --template-file ./$template_name-sed-template.yaml --s3-bucket $bucket_name --output-template-file $packaged_name
+aws cloudformation package --template-file ./template-$template_name-sed.yaml --s3-bucket $bucket_name --output-template-file $packaged_name
 aws cloudformation deploy --template-file $packaged_name --stack-name $stack_name --parameter-overrides $param --capabilities CAPABILITY_IAM
